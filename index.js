cc741150313568b95b7e71853b46adc457eed9ac
Omar Youssef <omar@youssef.io>
2015-04-10 07:30:04 +0200

    Using the combination of PubSub channel, HashMap key/value and Lists to build the queue.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 //A proof of concept.
 //No code structure whatsoever applied
 
+var redis = require('redis');
+var redisClient = redis.createClient(6379, '10.129.123.83');
 var NRP = require('node-redis-pubsub')
   , config = { port: 6379       // Port of your remote Redis server
              , host: '10.129.123.83' // Redis server host, defaults to 127.0.0.1
@@ -10,36 +12,59 @@ require('http').globalAgent.maxSockets = 100000
 var request = require('request');
 
 
-nrp.on('request|*', function (data, channel) {
-	var t2 = Date.now();
-	var requestUUID = data.requestUUID;
-	var method      = data.method;
-	var host        = data.host;
-	var uri         = data.uri;
+nrp.on('request|*', function (notifyData, channel) {
 
-	var t3 = Date.now();
+	var tServiceNotify = Date.now();
 
-	request({
-		method: method,
-		uri: 'http://localhost'+uri
-	}, function(error, response, body){
-		var t4 = Date.now();
+	redisClient.lpop(channel+"-queue", function(err, requestUUID){
 
-		data.timing['t2'] = t2;
-		data.timing['t3'] = t3;
-		data.timing['t4'] = t4;
+		var tServiceQueuePop = Date.now();
 
-		nrp.emit('response|'+channel, { requestUUID: requestUUID, response: body, timing: data.timing });	
+		redisClient.hgetall("request-"+requestUUID, function(err, requestData){
+
+			var tServiceHashGet = Date.now();
+
+			var method      = requestData.method;
+			var host        = requestData.host;
+			var uri         = requestData.uri;
+
+			var t3 = Date.now();
+
+			var tServiceStartHttp = Date.now();
+			request({
+				method: method,
+				uri: 'http://localhost'+uri
+			}, function(error, response, body){
+				var tServiceGotHttp = Date.now();
+
+				var timing = {};
+
+				timing['gatewayGotRequest'] = notifyData.timing['gatewayGotRequest'];
+				timing['gatewayQueuePush'] = notifyData.timing['gatewayQueuePush'];
+				timing['gatewayHashSet'] = notifyData.timing['gatewayHashSet'];
+				timing['serviceNotify'] = tServiceNotify;
+				timing['serviceQueuePop'] = tServiceQueuePop;
+				timing['serviceHashGet'] = tServiceHashGet;
+				timing['serviceStartHttp'] = tServiceStartHttp;
+				timing['serviceGotHttp'] = tServiceGotHttp;
+				
+				nrp.emit('response|'+channel, { requestUUID: requestData.requestUUID, response: body, timing: timing });	
+
+				redisClient.del("request-"+requestUUID, function(err, reply) {
+				    //console.log(reply);
+				});
+			});
+		});
 	});
 });
 
 var http = require('http');
 
 http.createServer(function (req, res) {
-	console.log(req.url+'3.'+Date.now());
+	//console.log(req.url+'3.'+Date.now());
 	setTimeout(function(){
 		res.writeHead(200, {'Content-Type': 'text/plain'});
-	  	res.end('Hello World\n');
-	  	console.log(req.url+'4.'+Date.now());
+	  	res.end('Hello World in '+req.url+'\n');
+	  	//console.log(req.url+'4.'+Date.now());
 	}, 350);
 }).listen(80);
